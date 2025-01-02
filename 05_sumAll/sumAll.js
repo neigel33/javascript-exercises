09b0ba12a50d517a26b227e1aa2689fe287bed08
neigel33 <neigel.adala@gmail.com>
2025-01-02 18:39:05 -0500

    Exercise 05_sumAll.js completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let sum = 0;
+    if (start > end) {
+        [start, end] = [end, start];
+      }
+    if (start < 0 || end <0 ) {
+        return 'ERROR';
+      }
+      if (!Number.isInteger(start) || !Number.isInteger(end)) { 
+        return 'ERROR';
+    }
+    if (typeof start !== 'number' || typeof end !== 'number') {
+        return 'ERROR';
+    }
+    for (let i = start; i <= end; i++) {
+      sum += i;
+      }
+    
+    return sum;
+  }   
 
-};
 
+// create new arraw with the in between numbers
+// 
 // Do not edit below this line
 module.exports = sumAll;
