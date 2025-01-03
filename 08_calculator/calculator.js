24539a61f956e4de461a0e9e00eca93966577c3d
neigel33 <neigel.adala@gmail.com>
2025-01-03 17:38:22 -0500

    exercise 08_calculator.js completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,49 @@
-const add = function() {
+const add = function(var1, var2) {
+  let sum = var1 + var2; 
+  return sum;
+
 	
 };
 
-const subtract = function() {
+const subtract = function(var1, var2) {
+	let sum = var1 - var2;
+  return sum;
+};
+
+const sum = function(arr) {
+  let sum = 0;
+  if (arr.length < 0) {
+    return 0;
+  } else {
+    for (let i = 0; i < arr.length; i++) {
+      sum += arr[i];
+    }
+    return sum;
+  }
+  
 	
 };
 
-const sum = function() {
+const multiply = function(arr) {
+  let product = 1;  
+  for (let i = 0; i < arr.length; i++) {  
+    product *= arr[i];
+  }
+  return product;
+};
+
+const power = function(val, exp) {
+  let sum= val ** exp;
+  return sum;
 	
 };
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
+const factorial = function(int) {
+  if (int <= 1) {
+    return 1;
+} else {
+    return int * factorial(int - 1);
+}
 	
 };
 
