c572b60c4a612557fa03d4b525ed102d0c999c4d
neigel33 <neigel.adala@gmail.com>
2025-01-03 18:40:57 -0500

    exercise 09_palindromes.js completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const cleaneString = string.toLowerCase().replace(/[^a-z0-9]/gi, '');
+    return cleaneString.split('').reverse().join('') === cleaneString;
+   
 };
 
 // Do not edit below this line
