706f226115bbd595ba1b187619475762c6fbea13
neigel33 <neigel.adala@gmail.com>
2025-01-03 20:10:54 -0500

    Exercise 12_findTheOldest.js completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const currentYear = new Date().getFullYear();
+    return people.reduce((oldest, current) => {
+        // Assume current year if `yearOfDeath` is undefined
+        const oldestDeathYear = oldest.yearOfDeath || currentYear;
+        const currentDeathYear = current.yearOfDeath || currentYear;
 
+        const oldestAge = oldestDeathYear - oldest.yearOfBirth;
+        const currentAge = currentDeathYear - current.yearOfBirth;
+
+        return (currentAge > oldestAge) ? current : oldest;
+    });
 };
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
